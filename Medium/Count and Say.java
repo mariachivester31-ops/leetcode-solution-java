8ab5a12de823869f47f0711e2a0174b1352f0188
mariachivester31-ops <mariachivester31@gmail.com>
2026-03-04 16:43:21 +0530

    Update Count and Say.java

--- Medium/Count and Say.java
+++ Medium/Count and Say.java
@@ -1,1 +1,19 @@
 
+class Solution {
+    public String countAndSay(int n) {
+       String s = "1";
+        while (--n > 0) {
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < s.length(); i++) {
+                int count = 1;
+                while (i + 1 < s.length() && s.charAt(i) == s.charAt(i + 1)) {
+                    count++;
+                    i++;
+                }
+                sb.append(count).append(s.charAt(i));
+            }
+            s = sb.toString();
+        }
+        return s; 
+    }
+}
