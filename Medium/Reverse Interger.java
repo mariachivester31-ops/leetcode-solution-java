04f31c8a54239b4af70496a257f8a9f4814eb386
mariachivester31-ops <mariachivester31@gmail.com>
2026-03-04 15:46:28 +0530

    Create Reverse Interger.java

---
+++ Medium/Reverse Interger.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int reverse(int x) {
+        int reversedNum=0;
+        while (x != 0) {
+            int digit = x % 10; 
+            if (reversedNum > Integer.MAX_VALUE / 10 || (reversedNum == Integer.MAX_VALUE / 10 && digit > 7)) {
+                return 0; 
+            }
+            if (reversedNum < Integer.MIN_VALUE / 10 || (reversedNum == Integer.MIN_VALUE / 10 && digit < -8)) {
+                return 0; 
+            }
+            reversedNum = reversedNum * 10 + digit;
+            x /= 10; 
+        }
+        return reversedNum;
+    }
+}
