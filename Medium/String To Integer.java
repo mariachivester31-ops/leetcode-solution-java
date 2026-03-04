7f99e24190bd6555c2b4e71bb863d56d003a98f2
mariachivester31-ops <mariachivester31@gmail.com>
2026-03-04 15:50:12 +0530

    Create String To Integer.java

---
+++ Medium/String To Integer.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int myAtoi(String s) {
+        if (s == null || s.isEmpty()) return 0;
+        
+        int i = 0;
+        int n = s.length();
+
+        while (i < n && s.charAt(i) == ' ') {
+            i++;
+        }
+        
+        if (i == n) return 0; 
+        int sign = 1;
+        if (s.charAt(i) == '-') {
+            sign = -1;
+            i++;
+        } else if (s.charAt(i) == '+') {
+            i++;
+        }
+        long result = 0;
+        while (i < n && Character.isDigit(s.charAt(i))) {
+            result = result * 10 + (s.charAt(i) - '0');
+            if (sign == 1 && result > Integer.MAX_VALUE) return Integer.MAX_VALUE;
+            if (sign == -1 && -result < Integer.MIN_VALUE) return Integer.MIN_VALUE;
+            
+            i++;
+        }
+        
+        return (int) (sign * result);
+    }
+}
